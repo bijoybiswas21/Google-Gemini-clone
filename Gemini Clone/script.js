d134dd7ac83ddb480d97918056c3b3dff59f8493
Bijoy Biswas <156492281+bijoybiswas21@users.noreply.github.com>
2025-03-17 21:44:42 +0530

    Update script.js

--- Gemini Clone/script.js
+++ Gemini Clone/script.js
@@ -9,7 +9,7 @@ const clearChatButton = document.getElementById("deleteButton");
 let currentUserMessage = null;
 let isGeneratingResponse = false;
 
-const GOOGLE_API_KEY = "AIzaSyCTuIrMxHCi6Dp-N4h2cWCWm1idjxtZX_w";
+const GOOGLE_API_KEY = "AIzaSyB5s9TqMYR0G2YxxGrKxJxN38gygRsVGLY";
 const API_REQUEST_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${GOOGLE_API_KEY}`;
 
 // Load saved data from local storage
